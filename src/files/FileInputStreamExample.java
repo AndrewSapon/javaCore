8319e6fc48225d4afd3894e00d80f07f9fe431b4
Andrew <sapon_a@mail.ru>
2016-02-29 18:20:37 +0200

    refactoring

--- src/files/FileInputStreamExample.java
+++ src/files/FileInputStreamExample.java
@@ -1,2 +1,40 @@
 package files;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+   Поток побайтового чтения из файла
+ */
+
+public class FileInputStreamExample {
+    public static void main(String[] args) throws IOException {
+        InputStream is = new FileInputStream("file2.txt");
+        int example = 3;
+        int k;
+
+        if (example == 1) {
+            int codeChar = 10;
+            while (codeChar > -1) {
+                codeChar = is.read();
+                System.out.println(codeChar + " = [" + ((char) codeChar) + "]");
+            }
+        }
+        if (example == 2) {
+            byte[] buf = new byte[20];
+            k = is.read(buf);
+            System.out.println("k=" + k);
+            for (byte b : buf) System.out.println("[" + b + "]=[" + (char) (b) + "]");
+        }
+        if (example == 3) {
+            is.read();
+            int len = is.available();
+            System.out.println(len);
+            byte[] buf = new byte[len];
+            k = is.read(buf);
+            System.out.println("k=" + k);
+            for (byte b : buf) System.out.println("[" + b + "]=[" + (char) (b) + "]");
+        }
+    }
+}
--- src/files/FileOutputStreamExample.java
+++ src/files/FileOutputStreamExample.java
@@ -1,7 +1,19 @@
 package files;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 /**
- * Created by Andrew on 29.02.2016.
+   Поток побайтовой записи в файл
  */
+
 public class FileOutputStreamExample {
+    public static void main(String[] args) throws IOException {
+        OutputStream os = new FileOutputStream("file2.txt");
+        byte[] buf = {100, 101, 102};
+        os.write(123);
+        os.write(buf);
+        os.close();
+    }
 }
