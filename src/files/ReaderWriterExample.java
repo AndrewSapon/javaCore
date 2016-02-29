5763626a2d34804dd7628fa1d23dc5ba9ee4ab97
Andrew <sapon_a@mail.ru>
2016-02-29 16:08:27 +0200

    FileReader/FileWriter

--- src/files/ReaderWriterExample.java
+++ src/files/ReaderWriterExample.java
@@ -1,7 +1,26 @@
 package files;
 
-/**
- * Created by Andrew on 29.02.2016.
- */
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class ReaderWriterExample {
+    public static void main(String[] args) throws Exception {
+        String fileName = "D://Dell//Yo.txt";
+        FileWriter fw = new FileWriter(fileName);
+        fw.write("stroka YOoo\n15d\t9");
+        fw.write("stroka 2");
+        //fw.flush();
+        fw.close();
+
+        FileReader fr = new FileReader(fileName);
+        Scanner scan = new Scanner(fr);
+        int i = 0;
+        while (scan.hasNextLine()) {
+            System.out.println("строка с номером :" + i + ": " + scan.nextLine());
+            i++;
+        }
+        scan.close();
+    }
 }
+
