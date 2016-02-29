3460236bdc6bd0f7c01466f07b2b6077c8b386ea
Andrew <sapon_a@mail.ru>
2016-02-29 16:44:31 +0200

    refactoring

--- src/recursion/Task06.java
+++ src/recursion/Task06.java
@@ -1,7 +1,21 @@
 package recursion;
 
 /**
- * Created by Andrew on 29.02.2016.
+ * Написать рекурсивный метод, проверяющий является ли число простым.
  */
 public class Task06 {
+    public static void main(String... args) {
+        System.out.println(isSimple(19, 2));
+    }
+
+    public static boolean isSimple(int number, int i) {
+        if (number == 1) return false; // 1 - не простое число
+        if (number == 2) return true; // особый случай
+
+        if (number % i == 0) return false;
+
+        if (i <= Math.sqrt(number)) // проверяем не все делители а только до корня квадратного из числа
+            return isSimple(number, i + 1);
+        else return true;
+    }
 }
--- src/recursion/Task07.java
+++ src/recursion/Task07.java
@@ -1,7 +1,43 @@
 package recursion;
 
+import java.io.File;
+
 /**
- * Created by Andrew on 29.02.2016.
+   Написать метод, принимающий в качестве параметра строку с значением папки на компьютере.
+   С помощью рекурсии получить список всех файлов и всех папок этой папки с помощью рекурсии.
+   -book
+   --directory1
+   ----file1
+   ----file2
+   --directory2
+   ----directory3
+   ------directory4
+   --------directory5
+   ----------file3
  */
 public class Task07 {
+    public static void main(String[] args) {
+        Task07 tf = new Task07();
+        tf.getFileNameFromFolder("D://IDE");
+    }
+
+    static String tmp = "-";
+
+    public void getFileNameFromFolder(String folderName) {
+        File folder = new File(folderName);
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isDirectory()) {
+                tmp += "-";
+                System.out.print(tmp);
+                System.out.println(listOfFiles[i].getName());
+                getFileNameFromFolder(folderName + "/" + listOfFiles[i].getName());
+            } else {
+                System.out.print(tmp);
+                System.out.println(listOfFiles[i].getName());
+            }
+        }
+        tmp = tmp.substring(0, tmp.length() - 1);
+    }
 }
