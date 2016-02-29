bf9bdd18ad56c025af4432873d4ad8211a94605b
Andrew <sapon_a@mail.ru>
2016-02-29 15:36:44 +0200

    farmating code

--- src/files/Task01.java
+++ src/files/Task01.java
@@ -1,7 +1,25 @@
 package files;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
- * Created by Andrew on 29.02.2016.
+ Запросить с консоли путь папки и расширение. Вывести на экран все файлы с раширением из этой папки(без иерархии)..
  */
 public class Task01 {
+    public static void main(String... args) {
+        Scanner scan = new Scanner(System.in);
+
+        File f = new File(scan.next());
+        System.out.println(f);
+        File[] files = f.listFiles();
+        System.out.println(Arrays.toString(files));
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].getName().contains("txt"))
+                System.out.println(files[i].getName());
+        }
+    }
+
 }
+
--- src/files/Task02.java
+++ src/files/Task02.java
@@ -1,7 +1,52 @@
 package files;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 /**
- * Created by Andrew on 29.02.2016.
+ * SpecialThread. Запросить путь на папку с консоли, получить список всех папок заданной папки с учетом иерархии.
+ * Например, поиск в папке "book" даст 5 элементов:
+ * -book
+ * --directory1
+ * ----file1
+ * ----file2
+ * --directory2
+ * ----directory3
+ * ------directory4
+ * --------directory5
+ * ----------file3
  */
 public class Task02 {
+    public static void main(String... args) {
+        List<File> fileList;
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        File f = new File(s);
+        System.out.println("--" + s);
+        File[] files = f.listFiles();
+        fileList = Arrays.asList(files);
+
+        for (int i = 0; i < fileList.size(); i++) {
+            if (fileList.get(i).isFile())
+                System.out.println(fileList.get(i).getName());
+
+            if (fileList.get(i).isDirectory()) {
+                System.out.println("--" + fileList.get(i).getName());
+                fileList.get(i).getPath();
+                File n = new File(fileList.get(i).getPath());
+                File[] n2 = n.listFiles();
+
+                for (int j = 0; j < n2.length; j++) {
+                    if (n2[j].isFile())
+                        // System.out.println(Arrays.toString(n2));
+                        System.out.println("" + n2[j].getName());
+                    if (n2[j].isDirectory())
+                        System.out.println("-----" + n2[j].getName());
+                }
+
+            }
+        }
+    }
 }
