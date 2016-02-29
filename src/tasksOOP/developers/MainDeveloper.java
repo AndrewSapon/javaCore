ed08bbe90243c2ccef56a175870a1a5cc5f4dcb7
Andrew <sapon_a@mail.ru>
2016-02-29 12:43:28 +0200

    reformating code

--- src/tasksOOP/developers/MainDeveloper.java
+++ src/tasksOOP/developers/MainDeveloper.java
@@ -6,14 +6,13 @@ import java.util.Scanner;
 /**
  * Created by Andrew on 30.12.2015.
  */
-public class MainDeveloper
-{
-    public static void main(String...args){
+public class MainDeveloper {
+    public static void main(String... args) {
         MainDeveloper mainDeveloper = new MainDeveloper();
-            mainDeveloper.m();
+        mainDeveloper.m();
     }
-    public  void m () throws InputMismatchException
-    {
+
+    public void m() throws InputMismatchException {
         // Создаем обьект класса Программист - велосипедист
         CyclDeveloper cyclDeveloper = new CyclDeveloper();
         cyclDeveloper.setName("Вася - Велосипедист");
@@ -42,110 +41,107 @@ public class MainDeveloper
         publicTransportDeveloper.getCityWhereHeWorks();
 
 
-        String [] weatherTypes = {"0: Очень сухая погода", "1: Обычная погода", "2: Дождливая погода", "3: Выход из программы"};
+        String[] weatherTypes = {"0: Очень сухая погода", "1: Обычная погода", "2: Дождливая погода", "3: Выход из программы"};
         for (String elem : weatherTypes)
             System.out.println(elem);
 
         boolean runWhile = true;
-        while (runWhile)
-        {
+        while (runWhile) {
             Scanner scan = new Scanner(System.in);
             System.out.println("Введите цифру:");
-            try{
-               int k = scan.nextInt();
+            try {
+                int k = scan.nextInt();
 
-            if (k == 0)
-            {
-                motoDeveloper.printAboutMe();
-                motoDeveloper.earnings(0);
-                System.out.println();
-                cyclDeveloper.printAboutMe();
-                cyclDeveloper.earnings(0);
-                System.out.println();
-                publicTransportDeveloper.printAboutMe();
-                publicTransportDeveloper.earnings(0);
-                System.out.println("**********************************************Вывод: ");
+                if (k == 0) {
+                    motoDeveloper.printAboutMe();
+                    motoDeveloper.earnings(0);
+                    System.out.println();
+                    cyclDeveloper.printAboutMe();
+                    cyclDeveloper.earnings(0);
+                    System.out.println();
+                    publicTransportDeveloper.printAboutMe();
+                    publicTransportDeveloper.earnings(0);
+                    System.out.println("**********************************************Вывод: ");
 
-                if (cyclDeveloper.money > motoDeveloper.money && cyclDeveloper.money > publicTransportDeveloper.money){
-                    System.out.println("В очень сухую погоду выгоднее ездить на велике");
-                    System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
-                    System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
-                    System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
-                } else if (motoDeveloper.money > cyclDeveloper.money && motoDeveloper.money > publicTransportDeveloper.money){
-                    System.out.println("В очень сухую погоду выгоднее ездить на мотоцикле");
-                    System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
-                    System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
-                    System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
-                } else if (publicTransportDeveloper.money > motoDeveloper.money && publicTransportDeveloper.money > cyclDeveloper.money){
-                    System.out.println("В очень сухую погоду выгоднее ездить на общ. транспорте");
-                    System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
-                    System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
-                    System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
+                    if (cyclDeveloper.money > motoDeveloper.money && cyclDeveloper.money > publicTransportDeveloper.money) {
+                        System.out.println("В очень сухую погоду выгоднее ездить на велике");
+                        System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
+                        System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
+                        System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
+                    } else if (motoDeveloper.money > cyclDeveloper.money && motoDeveloper.money > publicTransportDeveloper.money) {
+                        System.out.println("В очень сухую погоду выгоднее ездить на мотоцикле");
+                        System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
+                        System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
+                        System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
+                    } else if (publicTransportDeveloper.money > motoDeveloper.money && publicTransportDeveloper.money > cyclDeveloper.money) {
+                        System.out.println("В очень сухую погоду выгоднее ездить на общ. транспорте");
+                        System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
+                        System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
+                        System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
+                    }
+                } else if (k == 1) {
+                    motoDeveloper.printAboutMe();
+                    motoDeveloper.earnings(1);
+                    System.out.println();
+                    cyclDeveloper.printAboutMe();
+                    cyclDeveloper.earnings(1);
+                    System.out.println();
+                    publicTransportDeveloper.printAboutMe();
+                    publicTransportDeveloper.earnings(1);
+                    System.out.println("**********************************************Вывод: ");
+
+                    if (cyclDeveloper.money > motoDeveloper.money && cyclDeveloper.money > publicTransportDeveloper.money) {
+                        System.out.println("В обычную погоду выгоднее ездить на велике");
+                        System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
+                        System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
+                        System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
+                    } else if (motoDeveloper.money > cyclDeveloper.money && motoDeveloper.money > publicTransportDeveloper.money) {
+                        System.out.println("В обычную погоду выгоднее ездить на мотоцикле");
+                        System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
+                        System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
+                        System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
+                    } else if (publicTransportDeveloper.money > motoDeveloper.money && publicTransportDeveloper.money > cyclDeveloper.money) {
+                        System.out.println("В обычную погоду выгоднее ездить на общ. транспорте");
+                        System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
+                        System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
+                        System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
+                    }
+                } else if (k == 2) {
+                    motoDeveloper.printAboutMe();
+                    motoDeveloper.earnings(2);
+                    System.out.println();
+                    cyclDeveloper.printAboutMe();
+                    cyclDeveloper.earnings(2);
+                    System.out.println();
+                    publicTransportDeveloper.printAboutMe();
+                    publicTransportDeveloper.earnings(2);
+                    System.out.println("**********************************************Вывод: ");
+
+                    if (cyclDeveloper.money > motoDeveloper.money && cyclDeveloper.money > publicTransportDeveloper.money) {
+                        System.out.println("В дождливую погоду выгоднее ездить на велике");
+                        System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
+                        System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
+                        System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
+                    } else if (motoDeveloper.money > cyclDeveloper.money && motoDeveloper.money > publicTransportDeveloper.money) {
+                        System.out.println("В дождливую погоду выгоднее ездить на мотоцикле");
+                        System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
+                        System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
+                        System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
+                    } else if (publicTransportDeveloper.money > motoDeveloper.money && publicTransportDeveloper.money > cyclDeveloper.money) {
+                        System.out.println("В дождливую погоду выгоднее ездить на общ. транспорте");
+                        System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
+                        System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
+                        System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
+                    }
+                } else if (k == 3) {
+                    System.out.println("Выход из программы");
+                    runWhile = false;
                 }
-            } else if (k == 1)
-            {
-                motoDeveloper.printAboutMe();
-                motoDeveloper.earnings(1);
-                System.out.println();
-                cyclDeveloper.printAboutMe();
-                cyclDeveloper.earnings(1);
-                System.out.println();
-                publicTransportDeveloper.printAboutMe();
-                publicTransportDeveloper.earnings(1);
-                System.out.println("**********************************************Вывод: ");
-
-                if (cyclDeveloper.money > motoDeveloper.money && cyclDeveloper.money > publicTransportDeveloper.money){
-                    System.out.println("В обычную погоду выгоднее ездить на велике");
-                    System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
-                    System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
-                    System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
-                } else if (motoDeveloper.money > cyclDeveloper.money && motoDeveloper.money > publicTransportDeveloper.money){
-                    System.out.println("В обычную погоду выгоднее ездить на мотоцикле");
-                    System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
-                    System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
-                    System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
-                } else if (publicTransportDeveloper.money > motoDeveloper.money && publicTransportDeveloper.money > cyclDeveloper.money){
-                    System.out.println("В обычную погоду выгоднее ездить на общ. транспорте");
-                    System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
-                    System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
-                    System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
-                }
-            } else if (k == 2){
-                motoDeveloper.printAboutMe();
-                motoDeveloper.earnings(2);
-                System.out.println();
-                cyclDeveloper.printAboutMe();
-                cyclDeveloper.earnings(2);
-                System.out.println();
-                publicTransportDeveloper.printAboutMe();
-                publicTransportDeveloper.earnings(2);
-                System.out.println("**********************************************Вывод: ");
-
-                if (cyclDeveloper.money > motoDeveloper.money && cyclDeveloper.money > publicTransportDeveloper.money){
-                    System.out.println("В дождливую погоду выгоднее ездить на велике");
-                    System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
-                    System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
-                    System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
-                } else if (motoDeveloper.money > cyclDeveloper.money && motoDeveloper.money > publicTransportDeveloper.money){
-                    System.out.println("В дождливую погоду выгоднее ездить на мотоцикле");
-                    System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
-                    System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
-                    System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
-                } else if (publicTransportDeveloper.money > motoDeveloper.money && publicTransportDeveloper.money > cyclDeveloper.money){
-                    System.out.println("В дождливую погоду выгоднее ездить на общ. транспорте");
-                    System.out.println("зароботок парня на общ. траспорте: " + publicTransportDeveloper.money);
-                    System.out.println("зароботок мотоциклиста: " + motoDeveloper.money);
-                    System.out.println("зароботок велосипедиста: " + cyclDeveloper.money);
-                }
-            } else if (k == 3){
-                System.out.println("Выход из программы");
-                runWhile = false;
-            }
-            }catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("Не правильно введено число");
             }
         }
 
-        }
     }
+}
 
