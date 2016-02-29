ce5c1729a1562e228a9c13f0286f9ff5379d2d92
Andrew <sapon_a@mail.ru>
2016-02-29 11:58:51 +0200

    encoding to utf-8

--- src/tasksOOP/bus/Bus.java
+++ src/tasksOOP/bus/Bus.java
@@ -1,7 +1,36 @@
 package tasksOOP.bus;
 
-/**
- * Created by Andrew on 29.02.2016.
- */
-public class Bus {
+
+public class Bus
+{
+    private int countHuman;
+    private String bussType;
+
+
+    public String getBussType() {return bussType;}
+    public void setBussType(String bussType) {this.bussType = bussType;}
+
+    public void addHuman(int countHuman)
+    {
+        if (countHuman >= 0 && (this.countHuman + countHuman) < 50){
+            this.countHuman += countHuman;
+            System.out.println("Зашло " + countHuman + " человек");
+        } else if (countHuman >= 0 && (this.countHuman + countHuman) >= 50){
+            System.out.println("В автобусе " + this.countHuman + " человек, еще " + countHuman + " человек зайти не может");
+        } else if (countHuman < 0 && (this.countHuman + countHuman) >= 0){
+            this.countHuman += countHuman;
+            System.out.println("Вышло " + (-countHuman) + " человек");
+        } else if (countHuman < 0 && (this.countHuman + countHuman) < 0){
+            System.out.println("В автобусе " + this.countHuman + " человек, " + (-countHuman) + " людей выйти не может");
+        }
+    }
+
+    public void printAboutMe()
+    {
+        String [] operationNames = {"+(int) Люди заходят", "-(int) Люди выходят"};
+        for (String elem: operationNames)
+            System.out.println(elem);
+        if (this.countHuman == 0) {System.out.println("В автобусе никого нету - выйти никто не может");}
+        else {System.out.println("Сейчас в автобусе " + this.countHuman + " человек");}
+    }
 }
--- src/tasksOOP/bus/MainBus.java
+++ src/tasksOOP/bus/MainBus.java
@@ -1,7 +1,44 @@
 package tasksOOP.bus;
 
+import java.util.Scanner;
+
 /**
- * Created by Andrew on 29.02.2016.
+ Задача Автобус
+ (Bus и MainBus).
+ Написать класс Bus:
+ - с полем целочисленным countHuman (количество пассажиров)
+ - с метод void addHuman(int countHuman) (+ или - пассажиров)
+ - с методом printAboutMe (метод выводит количество пассажиров)
+ Также в методе addHuman, в случае уменьшения количества людей, не допускать, чтобы количество пассажиров было меньше 0.
+ Сделать консольное меню.
  */
-public class MainBus {
+public class MainBus
+{
+    public static void main (String [] args)
+    {
+        Bus bus = new Bus();
+        bus.setBussType("Богдан");
+        System.out.println("Автобус, типа: " + bus.getBussType());
+        System.out.println("Вместимость: 50 человек");
+
+        boolean runWhile = true;
+        while (true)
+        {
+            Scanner scan = new Scanner(System.in);
+            bus.printAboutMe();
+            try
+            {
+                System.out.println("Введите кол-во людей: ");
+                int k = scan.nextInt();
+                bus.addHuman(k);
+
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+
+            }
+        }
+    }
 }
+
