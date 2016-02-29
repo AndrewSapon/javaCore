3f4c7c173ff932ef3485e65a04267de24a95bda8
Andrew <sapon_a@mail.ru>
2016-02-29 13:36:02 +0200

    get/set

--- src/tasksOOP/plane/MainPlane.java
+++ src/tasksOOP/plane/MainPlane.java
@@ -1,7 +1,33 @@
 package tasksOOP.plane;
 
+import java.util.Scanner;
+
 /**
- * Created by Andrew on 29.02.2016.
+ * Created by Andrew on 29.12.2015.
  */
-public class MainPlane {
+public class MainPlane
+{
+    public static void main (String [] args)
+    {
+        Plane plain = new Plane();
+        plain.setPlaneType("AN - 24");
+        plain.setAirLinesType("Aerosvit");
+        System.out.println("Самолет " + plain.getPlaneType() + ", авиакомпании " + plain.getAirLinesType());
+
+        Scanner scan = new Scanner(System.in);
+        boolean runWhile = true;
+
+        while (runWhile)
+        {
+            plain.printAboutMe();
+            int k = scan.nextInt();
+
+            switch (k)
+            {
+                case 0: plain.up(); System.out.println(); break;
+                case 1: plain.down(); System.out.println(); break;
+                case 2: runWhile = false; System.out.println("Выход из программы");
+            }
+        }
+    }
 }
--- src/tasksOOP/plane/Plane.java
+++ src/tasksOOP/plane/Plane.java
@@ -1,7 +1,66 @@
 package tasksOOP.plane;
 
 /**
- * Created by Andrew on 29.02.2016.
+ * Created by Andrew on 29.12.2015.
  */
-public class Plane {
+public class Plane
+{
+    private String planeType;
+    private String airLinesType;
+    public int planeStatus;
+    //  1 - /\ - вверху
+    // 0 - \/ - внизу
+
+
+    public void setPlaneType(String planeType)
+    {
+        this.planeType = planeType;
+    }
+
+    public void setAirLinesType(String airLinesType)
+    {
+        this.airLinesType = airLinesType;
+    }
+
+    public String getPlaneType()
+    {
+
+        return planeType;
+    }
+
+    public String getAirLinesType()
+    {
+        return airLinesType;
+    }
+
+    public void up()
+    {
+        if (planeStatus == 1){
+            planeStatus = 0;
+            System.out.println("Я сел");
+        } else {
+            System.out.println("Я не могу сесть, так как и так внизу");
+        }
+    }
+
+    public void down()
+    {
+        if (planeStatus == 0){
+            planeStatus = 1;
+            System.out.println("Я взлетел");
+        } else {
+            System.out.println("Я не могу взлетеь, так как и так лечу");
+        }
+
+    }
+
+    public void printAboutMe()
+    {
+        String [] operationNames = {"0: Взлететь", "1: Сесть"};
+        for (String elem: operationNames)
+            System.out.println(elem);
+
+        if (planeStatus == 0) System.out.println(" Текущий статус - down");
+        else System.out.println(" Текущий статус - up");
+    }
 }
