dbd3e3c745239ceff781928b4dc0d4935c75f193
Andrew <sapon_a@mail.ru>
2016-02-29 16:27:39 +0200

    reformating code

--- src/recursion/Task01.java
+++ src/recursion/Task01.java
@@ -1,7 +1,19 @@
 package recursion;
 
 /**
- * Created by Andrew on 29.02.2016.
+ * Написать рекурсивный метод, который принимает в качестве параметров k.
+ * Метод выводит числа от k до 2.
  */
 public class Task01 {
+    public static void main(String... args) {
+        int k = 27;
+        f(k);
+    }
+
+    public static void f(int k) {
+        if (k <= 1)
+            return;
+        System.out.println(k);
+        f(k - 1);
+    }
 }
