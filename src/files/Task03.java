9ea8b275e1932a4a21fca1a69930016824902170
Andrew <sapon_a@mail.ru>
2016-02-29 16:11:42 +0200

    refactoring

--- src/files/Task03.java
+++ src/files/Task03.java
@@ -1,7 +1,28 @@
 package files;
 
+import java.io.FileWriter;
+
 /**
- * Created by Andrew on 29.02.2016.
+  Написать метод, который заполняет файл четными числами от k1 до к2.
+  Все числа в отдельной строке.
+  Этот метод принимает имя файла , к1 и к2 в качестве параметра.
  */
 public class Task03 {
+    public static void main(String... args) throws Exception {
+        String sFileName = "D://Dell//Yo.txt";
+        int k1 = 10;
+        int k2 = 30;
+        myMethod(sFileName, k1, k2);
+    }
+
+    public static void myMethod(String sFileName, int k1, int k2) throws Exception {
+
+        FileWriter fr = new FileWriter(sFileName);
+        for (int i = k1; i <= k2; i++) {
+            if (k1 % 2 == 0)
+                fr.write(k1 + "\n");
+            k1++;
+        }
+        fr.close();
+    }
 }
