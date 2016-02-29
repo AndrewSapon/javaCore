c13723236b9b7d9a0386999e2d528dcb258cbd0d
Andrew <sapon_a@mail.ru>
2016-02-29 16:32:19 +0200

    refactoring

--- src/recursion/Task02.java
+++ src/recursion/Task02.java
@@ -1,7 +1,19 @@
 package recursion;
 
 /**
- * Created by Andrew on 29.02.2016.
+ Написать рекурсивный метод, который принимает в качестве параметров k.
+ Метод выводит числа от 2 до k.
  */
 public class Task02 {
+    public static void main(String... args) {
+        int k = 27;
+        f(k);
+    }
+
+    public static void f(int k) {
+        if (k <= 1)
+            return;
+        f(k - 1);
+        System.out.println(k);
+    }
 }
