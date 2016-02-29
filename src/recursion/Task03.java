2793b2c3225e9792f08718a70c16c07a3f84b2d3
Andrew <sapon_a@mail.ru>
2016-02-29 16:41:05 +0200

    refactoring

--- src/recursion/Task03.java
+++ src/recursion/Task03.java
@@ -1,7 +1,20 @@
 package recursion;
 
 /**
- * Created by Andrew on 29.02.2016.
+   Написать метод, который считает факториал для числа n.
  */
 public class Task03 {
+    public static void main(String... args) {
+        int k = 5;
+        System.out.println(fact(k));
+    }
+
+    public static long fact(long k) {
+        if (k < 0)
+            throw new IllegalArgumentException();
+        if (k <= 1)
+            return 1;       // условие выхода из рекурсии
+        else
+            return k * fact(k - 1); // метод вызывает сам себя
+    }
 }
--- src/recursion/Task05.java
+++ src/recursion/Task05.java
@@ -1,7 +1,23 @@
 package recursion;
 
 /**
- * Created by Andrew on 29.02.2016.
+   Написать метод, который выводит n-ный член последовательности Фибоначчи.
  */
 public class Task05 {
+    public static void main(String[] args) {
+        f(5);
+    }
+
+    public static int f(int x) {
+        int result;
+        System.out.print(" " + x);
+        if (x == 0) {
+            result = 0;
+        } else if (x == 1) {
+            result = 1;
+        } else {
+            result = f(x - 2) + f(x - 1);
+        }
+        return result;
+    }
 }
