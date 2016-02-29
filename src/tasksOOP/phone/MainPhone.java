73ea3d8dc6339298483fa7f86c1ef663e3291218
Andrew <sapon_a@mail.ru>
2016-02-29 14:19:22 +0200

    refactoring

--- src/tasksOOP/phone/MainPhone.java
+++ src/tasksOOP/phone/MainPhone.java
@@ -1,7 +1,36 @@
 package tasksOOP.phone;
 
 /**
- * Created by Andrew on 29.02.2016.
+ * Телефон:
+ * Создать класс Phone с полями:
+ * - static Phone[] phones = new Phone[10];
+ * - String myNumber
+ * - String marka
+ * Изначально phones не содержит телефонов, все телефоны добавляются через конструктор.
+ * При создании нового объекта-телефона, мы должны указать его номер и он вносится в базу номеров phones.
+ * В этот класс добавить метод:
+ * - isNumberExist(String number) - проверяет наличие телефона в базе
+ * - call(String number) - выводит на экран "телефон такой-то(номер+марка) звонит такому-то(номер+марка)"
+ * - getRandomPhone() - возвращает случайный телефон из базы телефонов
+ * Ограничение в 10 телефонов.
+ * В MainPhone:
+ * - создать несколько телефонов с какими-то номерами
+ * - попробовать проверить наличие существующего и несуществующего номера.
+ * - для случайного телефона вызвать метод call и пусть звонит на случайный телефон.
  */
 public class MainPhone {
+    public static void main(String[] args) {
+        Phone phone1 = new Phone("0938764532", "Nokia");
+        Phone phone2 = new Phone("0938764535", "Samsung");
+
+        for (Phone elem : Phone.getPhones())
+            System.out.println(elem);
+
+        System.out.println("Есть ли телефон в базе? - " + Phone.isNumberExist("0938764535"));
+        System.out.println("Случайный телефон из базы: " + Phone.getRandomPhone().toString());
+        System.out.println("Кол-во телефонов в базе: " + Phone.getCountPhones());
+        phone1.call(Phone.getRandomPhone().getMyNumber());
+    }
+
 }
+
--- src/tasksOOP/phone/Phone.java
+++ src/tasksOOP/phone/Phone.java
@@ -1,7 +1,61 @@
 package tasksOOP.phone;
 
-/**
- * Created by Andrew on 29.02.2016.
- */
+import java.util.Random;
+
+
 public class Phone {
+    private static Phone[] phones = new Phone[10];
+
+    private String myNumber;
+    private String marka;
+    private static int countPhones;
+
+    // Метод проверяет есть ли телефон в базе
+    public static boolean isNumberExist(String number) {
+        boolean a = false;
+
+        for (int i = 0; i < phones.length; i++) {
+            if (phones[i] != null && (phones[i].getMyNumber()).equals(number)) {
+                a = true;
+                break;
+            } else a = false;
+        }
+        //  System.out.print("Есть ли телефон в базе? - ");
+        return a;
+    }
+
+    // Возвращает случайный телефон из базы
+    public static Phone getRandomPhone() {
+        Random random = new Random();
+        // Random  создает рендомные значения int nextInt(int n)  от 0 до n
+        // Так как countPhones - счетчик созданных обьектов класса Phone, используем его для задания кол-ва обьектов из которых выбирать n
+        Phone p = phones[random.nextInt(countPhones)];
+        return p;
+    }
+
+    // call(String number) - выводит на экран "телефон такой-то(номер+марка) звонит такому-то(номер+марка)"
+    public void call(String number) {
+        if (isNumberExist(number))
+            if (!number.equals(this.getMyNumber()))
+                System.out.println("Телефон, марка:" + this.getMarka() + ", номер: " + this.getMyNumber() + " звонит на телефон" + number);
+    }
+
+    // в Конструкторе, сразу добавляем телефон в базу, при создании
+    // countPhones - статический счетчик созданных экземпляров
+    public Phone(String myNumber, String marka) {
+        this.myNumber = myNumber;
+        this.marka = marka;
+        for (int i = 0; i < phones.length; i++) {
+            if (phones[i] == null) {
+                phones[i] = this;
+                break;
+            }
+        }
+        countPhones++;
+    }
+
+    public String getMyNumber() {return myNumber;}
+    public String getMarka() {return marka;}
+    public static int getCountPhones() {return countPhones;}
+    public static Phone[] getPhones() {return phones;}
 }
