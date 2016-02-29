5d4422a33dd18aa6953123eb16e272e6bb9a83bd
Andrew <sapon_a@mail.ru>
2016-02-29 12:09:25 +0200

    reformat code

--- src/tasksOOP/bus/Bus.java
+++ src/tasksOOP/bus/Bus.java
@@ -1,36 +1,41 @@
 package tasksOOP.bus;
 
 
-public class Bus
-{
+public class Bus {
     private int countHuman;
     private String bussType;
 
 
-    public String getBussType() {return bussType;}
-    public void setBussType(String bussType) {this.bussType = bussType;}
+    public String getBussType() {
+        return bussType;
+    }
 
-    public void addHuman(int countHuman)
-    {
-        if (countHuman >= 0 && (this.countHuman + countHuman) < 50){
+    public void setBussType(String bussType) {
+        this.bussType = bussType;
+    }
+
+    public void addHuman(int countHuman) {
+        if (countHuman >= 0 && (this.countHuman + countHuman) < 50) {
             this.countHuman += countHuman;
             System.out.println("Зашло " + countHuman + " человек");
-        } else if (countHuman >= 0 && (this.countHuman + countHuman) >= 50){
+        } else if (countHuman >= 0 && (this.countHuman + countHuman) >= 50) {
             System.out.println("В автобусе " + this.countHuman + " человек, еще " + countHuman + " человек зайти не может");
-        } else if (countHuman < 0 && (this.countHuman + countHuman) >= 0){
+        } else if (countHuman < 0 && (this.countHuman + countHuman) >= 0) {
             this.countHuman += countHuman;
             System.out.println("Вышло " + (-countHuman) + " человек");
-        } else if (countHuman < 0 && (this.countHuman + countHuman) < 0){
+        } else if (countHuman < 0 && (this.countHuman + countHuman) < 0) {
             System.out.println("В автобусе " + this.countHuman + " человек, " + (-countHuman) + " людей выйти не может");
         }
     }
 
-    public void printAboutMe()
-    {
-        String [] operationNames = {"+(int) Люди заходят", "-(int) Люди выходят"};
-        for (String elem: operationNames)
+    public void printAboutMe() {
+        String[] operationNames = {"+(int) Люди заходят", "-(int) Люди выходят"};
+        for (String elem : operationNames)
             System.out.println(elem);
-        if (this.countHuman == 0) {System.out.println("В автобусе никого нету - выйти никто не может");}
-        else {System.out.println("Сейчас в автобусе " + this.countHuman + " человек");}
+        if (this.countHuman == 0) {
+            System.out.println("В автобусе никого нету - выйти никто не может");
+        } else {
+            System.out.println("Сейчас в автобусе " + this.countHuman + " человек");
+        }
     }
 }
